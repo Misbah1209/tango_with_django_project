e789548eb2a52c5cbe6fdbd980f8e0618ece8571
misbah shaikh <2796456S@student.gla.ac.uk>
2023-02-17 16:43:45 +0000

    ch5

--- populate_rango.py
+++ populate_rango.py
@@ -7,42 +7,35 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    #First, we will create lists of dictionaries containing the pages
-    #we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories.
-    # This might seem a little bit confusing, but it allows us to iterate
-    # through each data structure, and add the data to our models.
-    
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
-
+        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+        'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
-
+        'url':'http://www.tangowithdjango.com/'} ]
+    
     other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ] 
-
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
-
-    for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        'url':'http://flask.pocoo.org'} ]
+    
+    cats = {'Python': {'pages': python_pages, 'views': 128, "likes": 64},
+        'Django': {'pages': django_pages, "views" : 64, "likes": 32},
+        'Other Frameworks': {'pages': other_pages, "views": 32, "likes": 16} }
+  
+     for cat, cat_data in cats.items():
+        c= add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c,p['title'],p['url'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -55,8 +48,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
